e3fbabf3355110a304203b22bd180bf93b6dfb68
Nick Walker <nswalker@cs.washington.edu>
2023-12-07 09:52:05 -0800

    Add ASIN to CLI tester
    Web interface relies on this being set

--- aurmr_tasks/scripts/test_cli.py
+++ aurmr_tasks/scripts/test_cli.py
@@ -15,19 +15,20 @@ from aurmr_tasks.srv import StowRequest, PickRequest
 
 def get_items() -> List[Tuple[str, str]]:
     return [
-            ("4E", "testing"),
-            ("4F", "testing"),
-            ("3E", "testing"),
-            ("3F", "vitamins"),
-            ("3F", "lysol"),
-            ("2H", "airpods"),
-            ("2F", "mouthwash"),
-            ("2F", "soup"),
-            ("2F", "masks"),
-            ("1F", "box")]
+        ("4E", "mommys_bliss_melatonin", "B079TD7HG2"),
+            ("4E", "testing", ""),
+            ("4F", "testing", ""),
+            ("3E", "testing", ""),
+            ("3F", "vitamins", ""),
+            ("3F", "lysol", ""),
+            ("2H", "airpods", ""),
+            ("2F", "mouthwash", ""),
+            ("2F", "soup", ""),
+            ("2F", "masks", ""),
+            ("1F", "box", "")]
 
 @click.command()
-@choice_option('--items', type=click.Choice([f'{b}: {o}' for b,o in get_items()]), multiple=True)
+@choice_option('--items', type=click.Choice([f'{b}: {o} ({a})' for b,o,a in get_items()]), multiple=True)
 def cli(items):
 
     console = Console()
@@ -42,17 +43,19 @@ def cli(items):
 
     print(items)
     for i in items:
-        b, o = i.split(': ', maxsplit=1)
+        b, o_a = i.split(': ', maxsplit=1)
+        o, a = o_a.split(" (")
+        a = a[:-1]
         if questionary.confirm(f'Please confirm that {o} is stowed in bin {b}.').ask():
             stow(bin_id=b, object_id=o)
-            stowed_items.append((b, o))
+            stowed_items.append((b, o, a))
         else:
             console.print(f'Skipping item [orange]{o}[/orange]')
 
     console.rule('Picking')
-    for b,o in track(stowed_items, console=console):
+    for b,o,a in track(stowed_items, console=console):
         console.print(f'Picking item [orange]{o}[/orange] out of bin [teal]{b}[teal]')
-        pick(bin_id=b, object_id=o)
+        pick(bin_id=b, object_id=o, object_asin=a)
 
 
 if __name__ == "__main__":
